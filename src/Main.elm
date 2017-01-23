8559ac9abe1f088754e4714a6c2c745aacfdaea6
drathier <fille.haglund@hotmail.com>
2017-01-23 23:50:46 +0100

    Implement time formatting

--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,100 @@
+module Main exposing (..)
+
 import Html exposing (program, div, button, text)
 import Html.Events exposing (onClick)
-import Time exposing (Time, every, millisecond)
+import Time exposing (Time, every, hour, minute, second, millisecond, inHours, inMinutes, inSeconds)
 
 
 main =
-  program {
-    init = ({people=1, time=0}, Cmd.none),
-    view = view,
-    update = update,
-    subscriptions = subscriptions
-    }
+    program
+        { init = ( { people = 1, time = 0 }, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 view model =
-  div []
-    [ button [ onClick Increment ] [ text "+" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Decrement ] [ text "-" ]
-    ]
+    div []
+        [ div [] [ text <| String.join ":" <| List.map leftPad2 <| timeToIntList model.time ]
+        , div []
+            [ button [ onClick Increment ] [ text "+" ]
+            , div [] [ text (toString model) ]
+            , button [ onClick Decrement ] [ text "-" ]
+            ]
+        ]
 
 
-type alias Model = {
-  people : Int,
-  time : Float
-  }
+leftPad2 : Int -> String
+leftPad2 n =
+    if n == 0 then
+        "00"
+    else if n < 10 then
+        "0" ++ toString n
+    else
+        toString n
 
 
-type Msg = Increment | Decrement | Tick Time
+timeToIntList : Time -> List Int
+timeToIntList t =
+    timeToIntListImpl t ""
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+timeToIntListImpl : Time -> String -> List Int
+timeToIntListImpl t last =
+    let
+        d =
+            truncate <| (inHours t) / 24
+
+        h =
+            truncate <| inHours t
+
+        m =
+            truncate <| inMinutes t
+
+        s =
+            truncate <| inSeconds t
+    in
+        if d >= 1 then
+            [ d ] ++ timeToIntListImpl (t - ((toFloat d) * 24 * hour)) "d"
+        else if last == "d" || h >= 1 then
+            [ h ] ++ timeToIntListImpl (t - ((toFloat h) * hour)) "h"
+        else if last == "h" || m >= 1 then
+            [ m ] ++ timeToIntListImpl (t - ((toFloat m) * minute)) ""
+        else
+            [ s ]
+
+
+type alias Model =
+    { people : Int
+    , time : Time
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Tick Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      ({model | people = model.people + 1}, Cmd.none)
+    case msg of
+        Increment ->
+            if model.people < 1000 then
+                ( { model | people = model.people + 1 }, Cmd.none )
+            else
+                ( model, Cmd.none )
 
-    Decrement ->
-      if model.people > 1 then
-        ({model | people = model.people - 1}, Cmd.none)
-      else
-        (model, Cmd.none)
+        Decrement ->
+            if model.people > 1 then
+                ( { model | people = model.people - 1 }, Cmd.none )
+            else
+                ( model, Cmd.none )
 
-    Tick time ->
-      ({model | time = model.time + 1}, Cmd.none)
+        Tick t ->
+            ( { model | time = model.time + 1 * second }, Cmd.none )
+
 
 subscriptions model =
-  every (toFloat (1000//model.people) * millisecond) Tick
+    every (toFloat (1000 // model.people) * millisecond) Tick
